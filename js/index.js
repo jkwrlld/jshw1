99b427dc80a8b1c06051eed89b05c09efb579bdd
jkwrlld <egordav729@gmail.com>
2025-11-25 15:55:46 +0200

    .

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let age = 13;
+const name = "Yehor";
+const isStudent = true;
+let myString = "Життя не чекає, поки ти будеш готовий – дій зараз.";
+console.log(typeof age, typeof name, typeof isStudent);
+console.log(myString);
