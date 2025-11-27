6860316c10ab2a076c93356fbadba06016da9ef4
jkwrlld <egordav729@gmail.com>
2025-11-27 17:41:41 +0200

    .

--- js/index.js
+++ js/index.js
@@ -4,3 +4,14 @@ const isStudent = true;
 let myString = "Життя не чекає, поки ти будеш готовий – дій зараз.";
 console.log(typeof age, typeof name, typeof isStudent);
 console.log(myString);
+const myNumber = 15;
+const myTen = 10;
+const operator = prompt("Введи +");
+if (operator == "+") {
+  console.log(+myNumber + +myTen);
+}
+console.log(myNumber);
+
+let myNull = null;
+
+console.log(myNull);
